2506ff334e8bd354b73c1ae7cec5040cd2a06e65
chenwanzhen <cwz@matech.cn>
2020-07-15 22:16:14 +0800

    第一次提交

---
+++ vue-music/src/components/recommend/recommend.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    推荐页面
+  </div>
+</template>
+
+<script>
+import { getTopBanner } from "@/api/recommend";
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this._getTopBanner();
+  },
+  methods: {
+    _getTopBanner() {
+      console.log("进入方法");
+      const e = getTopBanner();
+      console.log(e);
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus"></style>
---
+++ vue-music/src/components/search/search.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+      搜索页面
+  </div>
+</template>
+
+<script>
+import { getTopBanner } from "@/api/recommend";
+export default {
+  data() {
+    return {};
+  },
+  created() {
+    this._getTopBanner();
+  },
+  methods: {
+    _getTopBanner() {
+      console.log("进入方法");
+      const e = getTopBanner();
+      console.log(e);
+    }
+  }
+};
+</script>
+
+<style lang="stylus" scoped rel="stylesheet/stylus">
+</style>
